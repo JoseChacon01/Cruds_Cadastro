a2f24f136a6c853af1b34325b54be634128d83a2
JoseChacon01 <josemoreirachacon06@gmail.com>
2022-08-30 14:05:46 -0300

    atualizacoes

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Produto #".models" pois é um arquivo do mesmo diretorio
 from .forms import ProdutoForm
 
@@ -10,12 +10,19 @@ def listar_produtos(request):
     }
     return render (request, 'produtos.html', contexto)
 
+def opcao_produto(request):
+    return render   (request, 'opcao.html')      
+
 def cadastrar_produto(request):
     form = ProdutoForm(request.POST or None)
+
+    if form.is_valid():#Se o formulario for preenchido e todos os dados forem validos, SALVE.
+        form.save()
+        return redirect('listar_produtos')
+
     contexto = {
         'form_produto': form
     }
     return render (request, 'produto_cadastrar.html', contexto)
 
-def opcao_produto(request):
-    return render   (request, 'opcao.html')  
+ 
